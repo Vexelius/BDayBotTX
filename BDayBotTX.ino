a7839513c4534171dc052dd36c75a38aff78b6c3
Vexelius <Vexelius@gmail.com>
2016-08-18 19:56:03 -0500

    Starting to build the Menu
    Code cleanup
    Screen refresh has its own function, which means that the delay used
    when pressing S or Z is not needed anymore
    Welcome screen implemented

--- BDayBotTX.ino
+++ BDayBotTX.ino
@@ -18,7 +18,7 @@ char KEYS[]= {
 };
 // Key codes:
 // U: Up, D: Down, R: Right, L: Left
-// S: Select Mode + || Z: Select Mode -
+// S: Select Mode - || Z: Select Mode +
 // Action buttons: A, B, C
 // M: Play Melody
 
@@ -39,20 +39,23 @@ boolean transmitData; // Enable the transmitter and send data
 boolean lcdRefresh;    // Controls the LCD's refresh rate
 int robotMode;  // Sets up the operation mode
 // 1: Expression Mode   3: Greeting Mode
-// 2: Candle Mode       4: Config Mode
+// 2: Candle Mode
 
 struct dataStruct {
   unsigned long timeCounter;  // Save response times
-  char keyPress;
-  boolean keypadLock;          // M Button
+  char keyPress;          // When a key is pressed, this variable stores its unique code
+  boolean keypadLock;     // When this flag is active, no input will be received fron the keypad
+  boolean configMode;     // This flag determines wheter the robot is in Config Mode or not
 } myData;                 // Data stream that will be sent to the robot
 
 void setup() {
   Serial.begin(57600);
   // Set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("TX Test");
+  // Print the welcome message
+  lcd.print("B-DayBot");
+  lcd.setCursor(8,2);
+  lcd.print("Ver. 0.2");
 
   robotMode = 1;
   lcdRefresh = true;
@@ -75,34 +78,35 @@ void setup() {
 }
 
 void loop() {
-  if(Keypad.Getkey())
-  {
-  myData.keyPress = Keypad.Getkey();
-  Serial.print(F("Key pressed "));
-  Serial.println(myData.keyPress);
+  if(lcdRefresh==true)
+  screenDraw();
   
-  if(myData.keyPress=='S')
+  
+  if(Keypad.Getkey())   // Only start the process when a key is pressed
+  {
+  myData.keyPress = Keypad.Getkey();  // Store the key code
+
+  // Keys to change the robot's operation mode: S and Z
+  if(myData.keyPress=='S')  // S Key: Select Mode -
   {
     robotMode -= 1;
+    
+    // If you press the key when you're on the first of three modes available
+    // loop back to the third mode
     if(robotMode<1) robotMode = 3;
-    delay(200);
-    lcdRefresh = true;
+    lcdRefresh = true;  //Refresh the LCD screen's content
   }
   
-  if(myData.keyPress=='Z')
+  if(myData.keyPress=='Z')  // Z Key: Select Mode +
   {
     robotMode += 1;
+    
+    // If you press the key when you're on the third of three modes available
+    // loop back to the first mode
     if(robotMode>3) robotMode = 1;
-    delay(200);
-    lcdRefresh = true;
+    lcdRefresh = true;  //Refresh the LCD screen's content
   }
 
-  if(lcdRefresh==true)
-  {
-  Serial.print("Current Mode: ");
-  Serial.println(robotMode);
-  lcdRefresh = false;
-  }
   
   if((robotMode==1)
   &&(myData.keyPress=='U' || myData.keyPress=='D' || myData.keyPress=='L' || myData.keyPress=='R' 
@@ -168,167 +172,27 @@ void loop() {
   transmitData = false;
   delay(200);
   }
-  delay(200);
+  delay(200); // This allows for the key to debounce. Without it, there's a chance that a wrong keycode will be sent
   }
 }
 
 
+void screenDraw() {
+  lcd.clear();
 
-
-/*
-#include <SPI.h>
-#include "RF24.h"
-#include <printf.h>
-#include <LiquidCrystal.h>
-
-
-bool radioNumber = 0;
-
-RF24 radio(7,8);
-LiquidCrystal lcd(9, 6, 5, 4, 3, 2);
-
-
-byte addresses[][6] = {"1Node","2Node"};
-
-// Used to control whether this node is sending or receiving
-bool role = 0;
-
-void setup() {
-  Serial.begin(57600);
-  printf_begin();
-  Serial.println(F("RF24/examples/GettingStarted"));
-  Serial.println(F("*** PRESS 'T' to begin transmitting to the other node"));
-  
-  radio.begin();
-
-  // Set the PA Level low to prevent power supply related issues since this is a
- // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
- radio.setDataRate(RF24_250KBPS);
-  radio.setPALevel(RF24_PA_HIGH);
-  radio.setCRCLength(RF24_CRC_16);
-  radio.setRetries(15,15);
-  radio.setPayloadSize(8);
-  
-  // Open a writing and reading pipe on each radio, with opposite addresses
-  if(radioNumber){
-    radio.openWritingPipe(addresses[1]);
-    radio.openReadingPipe(1,addresses[0]);
-  }else{
-    radio.openWritingPipe(addresses[0]);
-    radio.openReadingPipe(1,addresses[1]);
+  if(robotMode==1)
+  {
+    lcd.print("Expression Mode");
   }
-
+  if(robotMode==2)
+  {
+    lcd.print("Candle Mode");
+  }
+  if(robotMode==3)
+  {
+    lcd.print("Greeting Mode");
+  }
   
-  // Start the radio listening for data
-  radio.startListening();
-  radio.printDetails();
-
-    // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("Transmitting!");
+  lcdRefresh = false;
 }
-
-void loop() {
-  
-  
- 
-if (role == 1)  {
-    
-    radio.stopListening();                                    // First, stop listening so we can talk.
-    
-    
-    Serial.println(F("Now sending"));
-
-    unsigned long start_time = micros();                             // Take the time, and send it.  This will block until complete
-     if (!radio.write( &start_time, sizeof(unsigned long) )){
-       Serial.println(F("failed"));
-     }
-        
-    radio.startListening();                                    // Now, continue listening
-    
-    unsigned long started_waiting_at = micros();               // Set up a timeout period, get the current microseconds
-    boolean timeout = false;                                   // Set up a variable to indicate if a response was received or not
-    
-    while ( ! radio.available() ){                             // While nothing is received
-      if (micros() - started_waiting_at > 200000 ){            // If waited longer than 200ms, indicate timeout and exit while loop
-          timeout = true;
-          break;
-      }      
-    }
-        
-    if ( timeout ){                                             // Describe the results
-        Serial.println(F("Failed, response timed out."));
-    }else{
-        unsigned long got_time;                                 // Grab the response, compare, and send to debugging spew
-        radio.read( &got_time, sizeof(unsigned long) );
-        unsigned long end_time = micros();
-        
-        // Spew it
-        Serial.print(F("Sent "));
-        Serial.print(start_time);
-        Serial.print(F(", Got response "));
-        Serial.print(got_time);
-        Serial.print(F(", Round-trip delay "));
-        Serial.print(end_time-start_time);
-        Serial.println(F(" microseconds"));
-
-        lcd.clear();
-        lcd.print("Success!");
-        lcd.setCursor(0,1);
-        lcd.print("Ping time: ");
-        lcd.print(start_time);
-        
-    }
-
-    // Try again 1s later
-    delay(1000);
-  }
-
-
-
-
-
-  if ( role == 0 )
-  {
-    unsigned long got_time;
-    
-    if( radio.available()){
-                                                                    // Variable for the received timestamp
-      while (radio.available()) {                                   // While there is data ready
-        radio.read( &got_time, sizeof(unsigned long) );             // Get the payload
-      }
-     
-      radio.stopListening();                                        // First, stop listening so we can talk   
-      radio.write( &got_time, sizeof(unsigned long) );              // Send the final one back.      
-      radio.startListening();                                       // Now, resume listening so we catch the next packets.     
-      Serial.print(F("Sent response "));
-      Serial.println(got_time);  
-   }
- }
-
-
-
-
-
-
-  if ( Serial.available() )
-  {
-    char c = toupper(Serial.read());
-    if ( c == 'T' && role == 0 ){      
-      Serial.println(F("*** CHANGING TO TRANSMIT ROLE -- PRESS 'R' TO SWITCH BACK"));
-      role = 1;                  // Become the primary transmitter (ping out)
-    
-   }else
-    if ( c == 'R' && role == 1 ){
-      Serial.println(F("*** CHANGING TO RECEIVE ROLE -- PRESS 'T' TO SWITCH BACK"));      
-       role = 0;                // Become the primary receiver (pong back)
-       radio.startListening();
-       
-    }
-  }
-
-
-} // Loop
-*/
 
